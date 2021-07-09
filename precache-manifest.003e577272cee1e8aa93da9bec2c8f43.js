a7f61c550328f97e21079f264b5a34e1f1794b99
Frank McCallister <fmccallister@gmail.com>
2021-07-09 15:32:16 -0400

    Updates

---
+++ precache-manifest.003e577272cee1e8aa93da9bec2c8f43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/GF_Rewards/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "849b0955016e43a9971a",
+    "url": "/GF_Rewards/static/js/runtime~main.849b0955.js"
+  },
+  {
+    "revision": "cb81facf5af4c6614b9d",
+    "url": "/GF_Rewards/static/js/main.cb81facf.chunk.js"
+  },
+  {
+    "revision": "78b2498a7ca1c837268c",
+    "url": "/GF_Rewards/static/js/1.78b2498a.chunk.js"
+  },
+  {
+    "revision": "cb81facf5af4c6614b9d",
+    "url": "/GF_Rewards/static/css/main.2fa3594f.chunk.css"
+  },
+  {
+    "revision": "78b2498a7ca1c837268c",
+    "url": "/GF_Rewards/static/css/1.c96d8bec.chunk.css"
+  },
+  {
+    "revision": "64b64c305f23e30f36faf888f04e212d",
+    "url": "/GF_Rewards/index.html"
+  }
+];
